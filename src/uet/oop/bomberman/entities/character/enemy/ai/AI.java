c2051032915b0ea66a1888c9635f2d50140b7500
khanhhd57 <100121399+khanhhd57@users.noreply.github.com>
2022-10-30 23:31:32 +0700

    fix

--- src/uet/oop/bomberman/entities/character/enemy/ai/AI.java
+++ src/uet/oop/bomberman/entities/character/enemy/ai/AI.java
@@ -8,7 +8,6 @@ public abstract class AI {
 
 	/**
 	 * Thuật toán tìm đường đi
-	 * @return hướng đi xuống/phải/trái/lên tương ứng với các giá trị 0/1/2/3
 	 */
 	public abstract int calculateDirection();
 }
